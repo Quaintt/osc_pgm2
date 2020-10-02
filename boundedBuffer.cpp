1f01882ba4790283d2f8c8787cb211b67fd36b71
Quaintt <htwerner01@gmail.com>
2020-10-01 23:23:37 -0500

    nightmare

--- boundedBuffer.cpp
+++ boundedBuffer.cpp
@@ -3,18 +3,22 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
 
 //colors because ofc.
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
 #define RESET "\x1B[0m"
 
 /*
@@ -51,11 +55,11 @@ void produ()
         sleep(0.1);
         //append();
 
-        int value1, value2, value3; 
+        int value1, value2, value3;
         sem_getvalue(&s, &value1);
         sem_getvalue(&n, &value2);
         sem_getvalue(&e, &value3);
-        printf(BLU "[produ]\t" CYN "Sending... One: %d N: %d e: %d\n" RESET, value1, value2, value3);
+        printf(BLU "[produ]\t" CYN "Sending... One: %d N: %d e: %d" RESET "\n", value1, value2, value3);
 
         sem_post(&s);
         sem_post(&n);
@@ -68,7 +72,7 @@ void produ()
         kill(pid + p, SIGKILL);
     }
     */
-   //kill(pid, SIGKILL);
+    //kill(pid, SIGKILL);
 }
 
 int consu()
@@ -78,31 +82,30 @@ int consu()
     bool isOneMillion = false;
     while (!isOneMillion)
     {
-        //printf(GRN "[consu %d]\t" RESET "waiting...\n", pid);
-        //sem_wait(&e);
-        //sem_wait(&s);
-        if (sem_wait(&e) != 0 || sem_wait(&s) != 0){
+        if (sem_wait(&e) != 0 || sem_wait(&s) != 0)
+        {
             printf(GRN "[consu %d]\t" RESET "waiting...\n", pid);
         }
         //sleep(0.05);
         sem_post(&s);
         sem_post(&n);
-        
-        int value1, value2, value3; 
+
+        int value1, value2, value3;
         sem_getvalue(&s, &value1);
         sem_getvalue(&n, &value2);
         sem_getvalue(&e, &value3);
         //printf(GRN "[consu %d]\t" RESET "...recieved", pid);
         //printf(CYN "\tOne: %d N: %d e: %d\n" RESET, value1, value2, value3);
-
+        
         obtained++;
-        if(value2 >= totalItems) isOneMillion = true;
-        //if(!isOneMillion){isOneMillion = true;}
+        if (value2 >= totalItems)
+            isOneMillion = true;
     }
     return obtained;
 }
 
-void spawnConsumers(int c){ //creates c + 2 consumers???
+void spawnConsumers(int c)
+{ //creates c + 2 consumers???
     /*
     if (fork() == 0)
     {
@@ -122,58 +125,113 @@ void spawnConsumers(int c){ //creates c + 2 consumers???
     }
     */
 
-    for (c; c > 0; c--){
-        pid_t pid = fork();
-        if (pid == 0) { //child
-            pid = getpid();
-            printf(RED "Created Consumer %d! ~ PID: %d\n" RESET, c, pid);
+    //TODO: Change stolen code
+    pid_t consu_pid;
+    for (c; c > 0; c--)
+    {
+        //pid_t pid = fork();
+        consu_pid = fork();
+        if (consu_pid == 0)
+        { //child
+            pid_t temp_pid = getpid();
+            printf(RED "Created Consumer %d! ~ PID: %d\n" RESET, c, temp_pid);
             int output = consu();
-            printf("Consumer " RED "%d" RESET " ended. Total items obtained: " RED "%d\n" RESET, pid, output);
+            printf("Consumer " RED "%d" RESET " ended. Total items obtained: " RED "%d\n" RESET, temp_pid, output);
             break;
         }
-        else if (pid < 0){ //error?
+        else if (consu_pid < 0)
+        { //error?
             //unlink semaphore
             //close semaphore
         }
     }
+
+    while (consu_pid = waitpid(-1, NULL, 0))
+    {
+        if (errno == ECHILD)
+        {
+            break;
+        }
+    }
+    printf(GRN "~~~ All children have ended. ~~~\n" RESET);
 }
 
 int main()
 {
-    sem_init(&s, 0, 1); //enforces mutual exclusion
+    sem_init(&s, 0, 1);            //enforces mutual exclusion
     sem_init(&n, 0, sizeOfBuffer); //size of buffer
-    sem_init(&e, 0, totalItems); //tracks empty spaces
+    sem_init(&e, 0, totalItems);   //tracks empty spaces
 
-    
-    if (fork() == 0)
+    pid_t gigafork = fork();
+    if (gigafork == 0)
     {
-        //child
+        //giga-child that handles all of the different elements
         pid_t pid = getpid();
-        printf(RED "Created original Consumer! ~ PID: %d\n" RESET, pid);
+        printf(GRN "Created original Consumer! ~ PID: %d\n" RESET, pid);
         spawnConsumers(10);
-        printf(RED "Parent finished loop! ~ PID: %d\n" RESET, pid);
+        printf(GRN "Parent finished loop! ~ PID: %d\n" RESET, pid);
         //kill(pid, SIGKILL);
         kill(getpid(), SIGTERM);
         return 2;
+        
+        /*
+        if (fork() == 0)
+        {
+            //child
+            pid_t pid = getpid();
+            printf(GRN "Created original Consumer! ~ PID: %d\n" RESET, pid);
+            spawnConsumers(10);
+            printf(GRN "Parent finished loop! ~ PID: %d\n" RESET, pid);
+            //kill(pid, SIGKILL);
+            kill(getpid(), SIGTERM);
+            return 2;
+        }
+        else
+        {
+            //parent
+            pid_t pid = getpid();
+            printf("Created Producer! ~ PID: %d\n", pid);
+            sleep(0.2);
+            produ();
+            sem_destroy(&s);
+            sem_destroy(&n);
+            sem_destroy(&e);
+            kill(getpid(), SIGTERM);
+            return 0;
+        }
+        */
     }
-    else
+    else if (gigafork != 0)
     {
-        //parent
+        //giga-parent
         pid_t pid = getpid();
         printf("Created Producer! ~ PID: %d\n", pid);
         sleep(0.2);
         produ();
+        
+        
+
+        //TODO: change stolen code
+        while (gigafork = waitpid(-1, NULL, 0))
+        {
+            if (errno == ECHILD)
+            {
+                break;
+            }
+        }
+        printf(GRN "~~~ All children have ended. ~~~\n" RESET);
+
+        // ADD CODE TO CLEAN UP EVERYTHING
+
         sem_destroy(&s);
         sem_destroy(&n);
         sem_destroy(&e);
         kill(getpid(), SIGTERM);
-        return 0;
+        
     }
-    printf("yo.\n");
-    kill(getpid(), SIGTERM);
+    //kill(getpid(), SIGTERM);
     return 1;
 }
-
 
 /*
 const int sizeOfBuffer = 1000;
