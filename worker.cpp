ffc75937fc2fd81e572abba9b7567e836b194077
Quaintt <htwerner01@gmail.com>
2020-10-03 22:43:36 -0500

    cleaned up code

--- worker.cpp
+++ worker.cpp
@@ -12,16 +12,11 @@
 #include <sys/mman.h>
 #include <stdlib.h>
 
-#define ITEMS 1000000
+#define ITEMS 100000
 
 //colors because ofc.
 #define RED "\x1B[31m"
 #define GRN "\x1B[32m"
-#define YEL "\x1B[33m"
-#define BLU "\x1B[34m"
-#define MAG "\x1B[35m"
-#define CYN "\x1B[36m"
-#define WHT "\x1B[37m"
 #define RESET "\x1B[0m"
 
 int pid[12];
@@ -29,7 +24,6 @@ const char ledger[] = "ledger";
 sem_t sem1, sem2;
 struct mem *hell; //TODO: Rename dumb var names
 const int totalItemsProduced = ITEMS;
-//pid_t pid;
 
 void spawnProcesses(int processes); //Forward declaration so the compiler doesn't scream at me.
 
@@ -40,7 +34,6 @@ struct mem
     sem_t accessReady;
     sem_t totalActionsPerformed;
     sem_t itemsAvailable;
-    size_t bytes_in_buffer;
     int bItems;
     char buffer[ITEMS]; //I'm just assuming he ment 1000 bytes and not 1000 items...
 };
@@ -79,14 +72,6 @@ void cleanup()
         waitpid(pid[j], NULL, 0); //reaper function
     }
 
-    while (waitpid(-1, NULL, 0) == 0) //I changed this so it probably doesn't work now
-    {
-        if (errno == ECHILD)
-        {
-            break;
-        }
-    }
-
     sem_close(&(hell->bufferReady));
     sem_close(&(hell->mSpaceReady));
     sem_close(&(hell->itemsAvailable));
@@ -107,32 +92,25 @@ int consumer(int id)
     int item;
     while (actionsPerformed < totalItemsProduced / 10)
     {
-        //wait until there are items to retrieve
-        //sem_wait(&(hell->itemsAvailable));
-
         //wait untill it's okay to access data
         sem_wait(&(hell->bufferReady));
 
-        //sleep(0.666);
-
         //how many items available?
         sem_wait(&(hell->itemsAvailable));
-        //sem_getvalue(&(hell->itemsAvailable), &item); //TODO: Make this wayyyy better
-        //item++;                                       //compensate for the change from the sem_wait
-        itemsRecieved++;                              //add to the items recieved count
-        
-        //read and modify(clear) data
+        itemsRecieved++; //add to the items recieved count
+
+        //wait for its turn to read/write
         sem_wait(&(hell->accessReady));
-        //printf(RESET "c%d:" GRN "[%d]%d" RESET, id, (item-1), hell->buffer[item-1]);
+
+        //read and modify/clear data
         item = hell->bItems;
         char k = hell->buffer[item - 1];
-        hell->buffer[item - 1] = id; //if one item available, go to slot zero
+        hell->buffer[item - 1] = id; //replace producer's message with consumer id to indicate message has been taken.
         hell->bItems -= 1;
         sem_post(&(hell->accessReady));
-        //printf(RED "[%d]%d" RESET, (item-1), hell->buffer[item-1]);
-        if (k != 63)
+        if (k != 63) //if message was not recived
         {
-            printf(RED "!!! consumer %d ~ buffer[%d]: " RESET, id, (item-1));
+            printf(RED "!!! consumer %d ~ buffer[%d]: " RESET, id, (item - 1));
             printf(RED "Existing Value %d\n" RESET, k);
             itemsRecieved--; //nullify the most recent item addition
         }
@@ -143,7 +121,7 @@ int consumer(int id)
         //tell producer that data has been changed
         sem_post(&(hell->mSpaceReady));
 
-        sleep(0.9); //aims to prevent the same consumer from reciving 2 items in a row
+        //sleep(0.9); //aims to prevent the same consumer from reciving 2 items in a row
     }
     return itemsRecieved;
 }
@@ -172,28 +150,15 @@ void producer()
         //placing data into shared memory buffer
         sem_wait(&(hell->accessReady)); //only one thing can write to mem at a time
         memcpy(&hell->buffer, message, strlen(message) - offset);
-        //memset(&hell->buffer, '?', s);
         itemsProduced++;
         hell->bItems += 1;
         sem_post(&(hell->accessReady));
 
         //increase the amount of items available
         sem_post(&(hell->itemsAvailable));
-        int output_text;
-        sem_getvalue(&(hell->itemsAvailable), &output_text);
-        // printf(RESET "ItmAvl: %d  ", output_text);
 
         //tell consumer that it is safe to access memory
         sem_post(&(hell->bufferReady));
-        sem_getvalue(&(hell->bufferReady), &output_text);
-        // printf("buffRdy: %d  ", output_text);
-
-        sem_getvalue(&(hell->mSpaceReady), &output_text);
-        // printf("sRdy: %d  ", output_text);
-
-        // printf("prod: %d  ", itemsProduced);
-
-        // printf("char at [0]: %d\n", hell->buffer[0]);
     }
 }
 
